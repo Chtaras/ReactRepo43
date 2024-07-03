27b846c24ea6d5a7e524d22636886e6dbd722720
Taras <taraschaikovskii556@gmail.com>
2024-07-04 00:02:29 +0200

    homework1

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
+  return (<body>
     <div className="App">
-      <h1> Hello React</h1>
+      <title>My Card</title>
+      <header>About Taras</header><main>
+      <h1> My card</h1>
+      <h2>Taras</h2>
+      <h2>Chaikovskyi</h2>
+      <div>
+        <ul>
+          <h4>Hobby</h4>
+        <div>
+          <li>
+            <a href="https://imgr1.auto-motor-und-sport.de/Autos-Ausland-Aufmacher-Collage-169FullWidth-45eb77d6-1663858.jpg">Cars
+            </a>
+            </li>
+            </div>
+        <div>
+          <li>
+            <a href="https://ethz.ch/de/studium/bachelor/studienstart/fachliche-vorbereitung/programmieren-anfaenger/_jcr_content/par/fullwidthimage/image.imageformat.930.1888441843.jpg">Programirung
+            </a>
+            </li>
+            </div>
+        <div>
+          <li>
+            <a href="https://dep.nj.gov/wp-content/uploads/njfw/fishing_freshwater_thumb.jpg">Fishing
+            </a>
+            </li>
+            </div>
+            </ul>
+      </div>
+      </main>
+      <footer>Goodbye</footer>
            
     </div>
+    </body>
   );
 }
 
